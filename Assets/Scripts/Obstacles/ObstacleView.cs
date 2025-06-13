d03a60107338fb368034c27bf31893c8aef9f3a6
sohamPatkar <spatkar398@gmail.com>
2025-06-13 21:28:21 +0530

    Added the game

---
+++ Assets/Scripts/Obstacles/ObstacleView.cs
@@ -0,0 +1,78 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Player;
+using UnityEngine;
+
+namespace Obstacle
+{
+    public class ObstacleView : MonoBehaviour
+    {
+        private ObstacleController obstacleController;
+        private SpriteRenderer colorOfSprite;
+
+        void Awake()
+        {
+            colorOfSprite = gameObject.GetComponent<SpriteRenderer>();
+        }
+
+        void Update()
+        {
+            obstacleController.ObstacleMovement(GameService.Instance.GetObstacleSpeed());
+        }
+
+        public void SetColor(ColorType colorType)
+        {
+            switch (colorType)
+            {
+                case ColorType.RED:
+                    colorOfSprite.color = Color.red;
+                    break;
+
+                case ColorType.GREEN:
+                    colorOfSprite.color = Color.green;
+                    break;
+
+                case ColorType.BLUE:
+                    colorOfSprite.color = Color.blue;
+                    break;
+
+                default:
+                    colorOfSprite.color = Color.red;
+                    break;
+            }
+
+        }
+
+        private void OnTriggerEnter2D(Collider2D other)
+        {
+            PlayerScript player = other.GetComponent<PlayerScript>();
+            if (player && (colorOfSprite.color == player.GetSpriteRenderColor()))
+            {
+                GameService.Instance.GetPlayer().OnHit();
+                GameService.Instance.GetPlayer().AddScore();
+                GameService.Instance.ChangeObstacleSpeed();
+                SoundManager.Instance.PlaySfxSound(SoundType.Collected);
+                GameService.Instance.SpawnObstacle();
+                GameService.Instance.GetPlayer().PlayParticleSysetem();
+                gameObject.SetActive(false);
+                obstacleController.ReturnToPool();
+            }
+            else
+            {
+                GameService.Instance.GetPlayer().OnHit();
+                GameService.Instance.GetPlayer().SetPlayerState(PlayerState.Dead);
+                SoundManager.Instance.PlaySfxSound(SoundType.Death);
+                GameService.Instance.SpawnObstacle();
+                gameObject.SetActive(false);
+            }
+        }
+
+        public void SetController(ObstacleController obstacleController)
+        {
+            this.obstacleController = obstacleController;
+        }
+    }
+}
+
+
---
+++ Assets/Scripts/Services/GameService.cs
@@ -0,0 +1,110 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Obstacle;
+using Player;
+using UnityEngine;
+
+public class GameService : MonoBehaviour
+{
+    [SerializeField] private ObstacleView obstacleView;
+    [SerializeField] private GameObject spawnPoint;
+    [SerializeField] private UIScript uIScript;
+    [SerializeField] private PlayerScript player;
+    [SerializeField] private List<ColorType> colorTypes;
+    [SerializeField] private float obstacleSpeed = 0;
+    [SerializeField] private float speedMultiplier = 0;
+
+    private static GameService instance;
+    public static GameService Instance { get { return instance; } }
+    public ObstaclePool obstaclePool;
+    public UIScript UIScript { get { return uIScript; } }
+    public event Action OnPlayerDeath;
+
+    void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    void Start()
+    {
+        UIScript.SetHighScoreText(GetHighScore());
+        obstaclePool = new ObstaclePool();
+        SpawnObstacle();
+    }
+
+    public void SpawnObstacle()
+    {
+        if (player.GetPlayerState() == PlayerState.Alive)
+        {
+            ColorType colorType = GetRandomColor();
+            ObstacleController obstacle = obstaclePool.GetObstacle(colorType, obstacleView);
+            obstacle.Activate();
+        }
+        else
+        {
+            SaveHighScore(player.GetPlayerScore());
+            OnPlayerDeath.Invoke();
+        }
+    }
+
+    public void SaveHighScore(int score)
+    {
+        int currentHighScore = PlayerPrefs.GetInt("HighScore", 0);
+        if (score > currentHighScore)
+        {
+            PlayerPrefs.SetInt("HighScore", score);
+            PlayerPrefs.Save();
+        }
+    }
+
+    private int GetHighScore()
+    {
+        return PlayerPrefs.GetInt("HighScore", 0);
+    }
+
+    public void ChangeObstacleSpeed()
+    {
+        obstacleSpeed += Time.deltaTime * speedMultiplier;
+    }
+
+    public float GetObstacleSpeed()
+    {
+        return obstacleSpeed;
+    }
+
+    private ColorType GetRandomColor()
+    {
+        return colorTypes[UnityEngine.Random.Range(0, colorTypes.Count)];
+    }
+
+    public PlayerScript GetPlayer()
+    {
+        return player;
+    }
+
+    public ObstacleController CreateObstacle(ColorType colorType, ObstacleView obstacleView)
+    {
+        switch (colorType)
+        {
+            case ColorType.RED:
+                return new RedObstacle(colorType, obstacleView, spawnPoint);
+
+            case ColorType.GREEN:
+                return new GreenObstacle(colorType, obstacleView, spawnPoint);
+
+            case ColorType.BLUE:
+                return new BlueObstacle(colorType, obstacleView, spawnPoint);
+
+            default:
+                return new ObstacleController(colorType, obstacleView, spawnPoint);
+        }
+    }
+}
